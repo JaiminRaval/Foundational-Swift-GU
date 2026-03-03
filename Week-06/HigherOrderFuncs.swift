bd7d7191fad775c93b7182b47e6428f64cbbc4cd
JaiminRaval <siliconvalley.jaimin@gmail.com>
2026-03-03 10:44:37 +0530

    added Big-O & HigherOrderFunc

--- Week-06/HigherOrderFuncs.swift
+++ Week-06/HigherOrderFuncs.swift
@@ -18,3 +18,9 @@ print(evens)  // Output: [2, 4]
 // The '0' is the starting value. $0 is the running total, $1 is the current number.
 let sum = numbers.reduce(0) { $0 + $1 }
 print(sum)  // Output: 15
+
+let bills = [
+    "Diet Coke 12.99",
+    "20.50 for Cookie",
+    " 52.78 for 4 slice of pizza",
+]
